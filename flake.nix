b923d7db9111c5979c52a0fc96093a65f5156171
Igor Gilmutdinov <bladoff@gmail.com>
2021-07-22 22:51:14 +0300

    21 page

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  description = "rust workspace";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    let
+      myapp = "poe-system";
+      rust-version = "1.53.0";
+    in flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [
+          rust-overlay.overlay
+          (self: super: rec {
+            rustc = self.rust-bin.stable.${rust-version}.default.override {
+              extensions =
+                [ "rust-src" "rustfmt-preview" "llvm-tools-preview" ];
+            };
+            cargo = rustc;
+          })
+        ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        lib = pkgs.lib;
+
+        buildInputs = with pkgs; [
+          sccache
+          sqlite
+          postgresql
+          mysql
+          openssl
+          pkg-config
+          cmake
+          zlib
+          gnumake
+          python3
+          jq
+          nixos-shell
+        ];
+        nativeBuildInputs = with pkgs; [ rustc cargo pkgconfig nixpkgs-fmt ];
+
+      in rec {
+        devShell = with pkgs;
+          mkShell {
+            name = "rust";
+            buildInputs = [ ] ++ buildInputs;
+            inherit nativeBuildInputs;
+
+            shellHook = ''
+              export RUSTC_WRAPPER="${pkgs.sccache}/bin/sccache"
+              export PATH=$PATH:$HOME/.cargo/bin
+            '';
+          };
+      });
+}
